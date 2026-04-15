eeaa20b80aef226e8a694c6264578bfa2e2a6187
ssawaul <ssawaul@gmail.com>
2026-04-15 18:35:33 +0900

    add  fun add

---
+++ calculator.py
@@ -0,0 +1,16 @@
+﻿def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("0으로 나눌 수 없습니다")
+    return a / b
