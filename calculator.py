89c4eefb47b5ad490a7eb1534682a3a2601ae1aa
ssawaul <ssawaul@gmail.com>
2026-04-15 19:01:09 +0900

    test code

--- calculator.py
+++ calculator.py
@@ -1,2 +1,9 @@
-def add(a, b):
+﻿def add(a, b):
     return a + b
+def subtract(a, b):
+    """a에서 b를 뺀 값을 반환합니다."""
+    return a - b
+
+def multiply(a, b):
+    """두 수의 곱을 반환합니다."""
+    return a * b
